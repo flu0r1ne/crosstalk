ee6c838a35276c3c0a0018bc495cb210be3f8fdd
flu0r1ne <flu0r1ne@flu0r1ne.net>
2024-08-01 13:13:50 -0700

    Add Configuration Subsystem
    Add support for configuring Crosstalk behavior through a declarative
    TOML configuration file. This update includes the following features:
    
    - Configuration of the default editor via the `editor` field.
    - Configuration of providers:
      + Modify provider priorities to control which providers are selected
      when using the `chat` command.
      + Change a provider's default model.
      + Enable the ability to disable providers or force them to be enabled,
      which triggers an error when the activation criteria are unmet.
      + Specify provider parameters, such as OpenAI's API key and Ollama's
      API URL.

--- src/registry/populate.rs
+++ src/registry/populate.rs
@@ -1,15 +1,18 @@
+use std::env::VarError;
+
+use die::die;
+
 use super::registry::{Error, ModelResolver, ModelSpec, Registry};
+use crate::config::{Config, RequestedProviderEnabled};
 use crate::providers::providers::{OllamaProvider, OpenAIProvider};
 use crate::providers::{ChatProvider, ErrorKind};
 
-async fn ollama_provider() -> Option<Box<OllamaProvider>> {
-    let ollama = OllamaProvider::new();
-
+async fn ollama_is_awake(ollama: &OllamaProvider) -> bool {
     let models = ollama.models().await;
 
     if let Err(err) = models {
         if matches!(err.kind(), ErrorKind::Connection | ErrorKind::TimedOut) {
-            return None;
+            return false;
         }
 
         panic!(
@@ -18,20 +21,95 @@ async fn ollama_provider() -> Option<Box<OllamaProvider>> {
         );
     }
 
-    Some(Box::new(ollama))
+    true
 }
 
-pub(crate) async fn populated_registry() -> Registry {
+const OPENAI_ENV_KEY_VAR: &'static str = "OPENAI_API_KEY";
+
+fn openai_api_key() -> Option<String> {
+    match std::env::var(OPENAI_ENV_KEY_VAR) {
+        Ok(api_key) => Some(api_key),
+        Err(err) => match err {
+            VarError::NotUnicode(_) => die::die!("error: failed to parse {}", OPENAI_ENV_KEY_VAR),
+            VarError::NotPresent => None,
+        },
+    }
+}
+
+/// Populate a registry with the available providers
+pub(crate) async fn populated_registry(config: &Config) -> Registry {
     let mut registry = Registry::new();
 
-    if let Some(provider) = ollama_provider().await {
-        registry.add_provider(provider, None);
+    {
+        let ollama = &config.providers.ollama;
+
+        let provider = match ollama.enabled {
+            RequestedProviderEnabled::Auto | RequestedProviderEnabled::Yes => {
+                if let Some(api_base) = &ollama.api_base {
+                    match OllamaProvider::with_api_base(api_base) {
+                        Ok(ollama) => Some(ollama),
+                        Err(err) => die::die!("ollama API base failed to parse: {}", err),
+                    }
+                } else {
+                    Some(OllamaProvider::new())
+                }
+            }
+            RequestedProviderEnabled::No => None,
+        };
+
+        match (provider, ollama.enabled) {
+            (Some(provider), RequestedProviderEnabled::Auto)
+                if ollama_is_awake(&provider).await =>
+            {
+                registry.add_provider(
+                    Box::new(provider),
+                    ollama.priority,
+                    ollama.default_model.clone(),
+                );
+            }
+            (Some(provider), RequestedProviderEnabled::Yes) => {
+                registry.add_provider(
+                    Box::new(provider),
+                    ollama.priority,
+                    ollama.default_model.clone(),
+                );
+            }
+            _ => {}
+        }
     }
 
-    if let Ok(api_key) = std::env::var("OPENAI_API_KEY") {
-        let openai_provider = Box::new(OpenAIProvider::with_api_key(&api_key));
+    {
+        let openai = &config.providers.openai;
+        let openai_env_var = openai_api_key();
 
-        registry.add_provider(openai_provider, None);
+        let api_key = if let Some(api_key) = &openai.api_key {
+            Some(api_key)
+        } else if let Some(api_key) = &openai_env_var {
+            Some(api_key)
+        } else {
+            None
+        };
+
+        let enabled = match openai.enabled {
+            RequestedProviderEnabled::Auto => {
+                // Enable if API key is present
+                api_key
+            }
+            RequestedProviderEnabled::Yes => {
+                if api_key.is_none() {
+                    die::die!("the \"openai\" provider is enabled but the API key is not defined, either add it to the config or define {}", OPENAI_ENV_KEY_VAR);
+                }
+
+                api_key
+            }
+            RequestedProviderEnabled::No => None,
+        };
+
+        if let Some(api_key) = enabled {
+            let provider = Box::new(OpenAIProvider::with_api_key(&api_key));
+
+            registry.add_provider(provider, openai.priority, openai.default_model.clone());
+        }
     }
 
     registry
